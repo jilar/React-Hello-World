f994641abfbc65b475b5b6a1082b4f8b0fc22aeb
jilar <j.ilar1215@hotmail.com>
2019-02-28 16:09:56 -0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+//import the react and reactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//example funtion
+function getButton(){
+  return "Click on me!";
+}
+//create a react component
+const App = () => {
+  const buttonText='Click Me!';
+
+  return (
+    <div>
+      <label className="label" htmlFor= "name"> Enter name:</label>
+      <input id= "name" type= "text"/>
+      <button style = {{backgroundColor: "blue", color: "white"}}>{getButton()}</button>
+    </div>
+  );
+}
+
+//take component and show it on screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
